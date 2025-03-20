4f64a5764be27a0f12c1997b3ad6378eb6ce5348
nicistey <nicisteylv@ya.ru>
2025-03-20 21:41:10 +0500

    добавить асинхронную обработку запросов + новый эндпоинт, для наглядности работы асинхронности

--- client.cpp
+++ client.cpp
@@ -8,20 +8,49 @@
 class templateClient{
 private:
 std::unique_ptr<templateProto::Greeter::Stub> stub;
+grpc::CompletionQueue cq;
+
+struct CallData {
+    templateProto::HelloRequest request;
+    templateProto::HelloReply reply;
+    std::unique_ptr<grpc::ClientContext> context;
+    grpc::Status status;
+    std::unique_ptr<grpc::ClientAsyncResponseReader<templateProto::HelloReply>> response_reader;
+};
 public:
     templateClient(std::shared_ptr<grpc::Channel> channel) : stub(templateProto::Greeter::NewStub(channel)) {}
 
-    std::string SayHello(const std::string& user) {
-        templateProto::HelloRequest request;
-        request.set_name(user);
-        templateProto::HelloReply reply;
+   void SayHello(const std::string& user) {
+
+        auto* call = new CallData;
+        call->request.set_name(user);
+        call->context = std::make_unique<grpc::ClientContext>();
+        call->response_reader = stub->AsyncSayHello(call->context.get(), call->request, &cq);
+        call->response_reader->Finish(&call->reply, &call->status, (void*)call);
+    }
+    void AsyncCompleteRpc() {
+        void* got_tag;
+        bool ok = false;
+        while(cq.Next(&got_tag, &ok)) {
+            CallData* call = static_cast<CallData*>(got_tag);
+            if(call->status.ok()) {
+                std::cout << "Server answered: " << call->reply.answer() << std::endl;
+            } else {
+                std::cout << "RPC failed" << std::endl;
+            }
+            delete call;
+        }
+    }
+    int TimeWorkServer() {
+        templateProto::Empty request;
+        templateProto::Seconds reply;
         grpc::ClientContext context;
-        grpc::Status status = stub->SayHello(&context, request, &reply);
+        grpc::Status status = stub->TimeWorkServer(&context, request, &reply);
         if(status.ok()) {
-            return reply.answer();
+            return reply.time();
         } else {
             std::cout << status.error_code() << ": " << status.error_message() << std::endl;
-            return "RPC failed";
+            return -1;
         }
     }
 };
@@ -29,15 +58,21 @@ public:
 void RunClient() {
     std::string server_address("localhost:50051");
     templateClient client(grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
+   
+    std::thread cq_thread(&templateClient::AsyncCompleteRpc, &client);
     while (true) {
-        std::string user;
+        std::string text;
         std::cout << "Enter name or /STOP: ";
-        std::cin >> user;
-        if(user == "/STOP") {
+        std::cin >> text;
+        if(text == "/STOP") {
             break;
         }
-        std::string reply = client.SayHello(user);
-        std::cout << "Server answered: " << reply << std::endl;
+        if(text == "/TIME") {
+            int time = client.TimeWorkServer();
+            std::cout << "Server time: " << time << std::endl;
+            continue;
+        }
+        client.SayHello(text);
     }
 }
 
--- server.cpp
+++ server.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
+#include <chrono>
 
 #include <grpcpp/grpcpp.h>
 #include "templateProto.grpc.pb.h"
 
 class templateServer final : public templateProto::Greeter::Service {
+private:
+    std::chrono::steady_clock::time_point serverStartTime;
+public:
+    templateServer(): serverStartTime(std::chrono::steady_clock::now()) {}
+
     grpc::Status SayHello(grpc::ServerContext* context, const templateProto::HelloRequest* request, templateProto::HelloReply* response) override {
         std::cout << "Request: " << request->name() << std::endl;
+        
+        //для наглядности работы асинхронных запросов
+        std::this_thread::sleep_for(std::chrono::seconds(20));
+
+        std::cout << "RUN"<<std::endl;
         std::string prefix("Hello ");
         if(request->name() == "error") {
             return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, "Invalid argument");
         }
         response->set_answer(prefix + request->name());
+        return grpc::Status::OK;
+    } 
+
+    grpc::Status TimeWorkServer(grpc::ServerContext* context, const templateProto::Empty* request, templateProto::Seconds* response) override {
+        std::chrono::steady_clock::time_point currentTime = std::chrono::steady_clock::now();
+        std::chrono::duration<double> timeSpan = std::chrono::duration_cast<std::chrono::duration<double>>(currentTime - serverStartTime);
+        response->set_time(timeSpan.count());
         return grpc::Status::OK;
     }
 };
